32e867713875acbfedafbf3bfce0d2397ae00861
anhilashkgeorge <user.abhilash@gmail.com>
2021-12-01 11:16:43 +0530

    first main commit

--- BankAccountModel/main.swift
+++ BankAccountModel/main.swift
@@ -5,7 +5,30 @@
 //  Created by Abhilash k George on 01/12/21.
 //
 
+
 import Foundation
+struct Bank {
+    func createAccount() {
+        
+    }
+}
+protocol CreateAccountProtocol {
+    var name: String {get}
+    var initialAmount: Double {get}
+    var accountNumber: CLong {get}
+    func createAccount(name: String, initialAmount: Double, accountNumber: CLong)
+}
 
-print("Hello, Earth!")
-
+struct User: CreateAccountProtocol {
+    var name: String
+    
+    var initialAmount: Double
+    
+    var accountNumber: CLong
+    
+    func createAccount(name: String, initialAmount: Double, accountNumber: CLong) {
+        //self.name = name
+        
+        
+    }
+}
